65eb04f8d10aa0d504b690428aac8e551363c0dd
Midhun Gaddipati <midhun.gaddipati@redbox.com>
2018-12-12 10:43:52 -0600

    Fixed Typo

--- tests/e2e/newReleasesTests.js
+++ tests/e2e/newReleasesTests.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "Validate new release - movies functionality": '' + function(browser) {
+    "Validate new release - movies functionality": function(browser) {
         signInPage = browser.page.loginPage()
         landingPage = browser.page.landingPage()
         newReleasesMoviesPage = browser.page.newReleasesMoviesPage()
@@ -12,7 +12,7 @@ module.exports = {
         browser.end();
     },
 
-    "Validate new release - games functionality": '' + function(browser) {
+    "Validate new release - games functionality": function(browser) {
         signInPage = browser.page.loginPage()
         landingPage = browser.page.landingPage()
         newReleasesGamesPage = browser.page.newReleasesGamesPage()
@@ -39,7 +39,7 @@ module.exports = {
         browser.end();
     },
 
-    "Validate new coming soon - games functionality": '' + function(browser) {
+    "Validate new coming soon - games functionality": function(browser) {
         signInPage = browser.page.loginPage()
         landingPage = browser.page.landingPage()
         newComingSoonGamesPage = browser.page.newComingSoonGamesPage()
